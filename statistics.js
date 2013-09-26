10af81baad62584ef31a497b21479ca8db31fb5a
Sina Mahmoodi <itz.s1na@gmail.com>
2013-09-26 18:09:45 +0330

    Fix nginx forwarding issue with remote address.

--- statistics.js
+++ statistics.js
@@ -4,15 +4,21 @@ var rdbLogger = config.rdbLogger;
 
 module.exports = function statistics() {
   return function (req, res, next) {
+    var ip = '';
+    if (req.headers['x-nginx-proxy'] == 'true') {
+      ip = req.headers['x-real-ip'];
+    } else {
+      ip = req.connection.remoteAddress;
+    }
     rdb.sismember(
       'stats:ip',
-      req.connection.remoteAddress,
+      ip,
       function (err, reply) {
         if (err || typeof reply === 'undefined') {
           console.log('[Statistics] No reply from redis.');
         } else {
           if (!reply) {
-            rdb.sadd('stats:ip', req.connection.remoteAddress, rdbLogger);
+            rdb.sadd('stats:ip', ip, rdbLogger);
             rdb.incr('stats:visit:total', rdbLogger);
           }
         }
