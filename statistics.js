bd812373ef4fb6dafd1a6c72e53f00e16a9ce708
Sina Mahmoodi <itz.s1na@gmail.com>
2013-09-26 17:58:36 +0330

    Add some basic statistics gatherings.

--- statistics.js
+++ statistics.js
@@ -1,6 +1,24 @@
+var config = require('./config');
+var rdb = config.rdb;
+var rdbLogger = config.rdbLogger;
+
 module.exports = function statistics() {
   return function (req, res, next) {
-    console.log('Remote address: ' + req.connection.remoteAddress + '  ' + req.path );
+    rdb.sismember(
+      'stats:ip',
+      req.connection.remoteAddress,
+      function (err, reply) {
+        if (err || typeof reply === 'undefined') {
+          console.log('[Statistics] No reply from redis.');
+        } else {
+          if (!reply) {
+            rdb.sadd('stats:ip', req.connection.remoteAddress, rdbLogger);
+            rdb.incr('stats:visit:total', rdbLogger);
+          }
+        }
+      }
+    );
+    rdb.incr('stats:hit:total', rdbLogger);
     next();
   };
 }
