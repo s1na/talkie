f8d39813fae18046a25f595d51fa23d3d5ce2764
Sina Mahmoodi <itz.s1na@gmail.com>
2013-09-29 19:12:43 +0330

    Add logger.

---
+++ logger.js
@@ -0,0 +1,34 @@
+
+module.exports.info = function (from, msg) {
+  setTimeout(function () {
+    log('info', from, msg);
+  }, 1);
+};
+
+module.exports.err = function (from, msg) {
+  setTimeout(function () {
+    log('err', from, msg);
+  }, 1);
+};
+
+function log(type, from, msg) {
+  var now = new Date(Date.now());
+  now = now.getFullYear() + '/' + now.getMonth() + '/' + now.getDate() +
+    ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+
+  if (typeof from === 'undefined') {
+    from = ''
+  } else {
+    from = '[' + from[0].toUpperCase() + from.slice(1) + ']';
+  }
+
+  if (typeof msg === 'object') {
+    msg = JSON.stringify(msg);
+  }
+
+  if (type == 'info') {
+    console.log(now + ' ' + from + ' ' + msg);
+  } else if (type == 'err') {
+    console.error(now + ' ' + from + ' ' + msg);
+  }
+}
